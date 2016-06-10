e09268c9a2b70d3f32f82278fed1b24e1c22f079
jezou <jefferson.zou@yale.edu>
2016-06-10 19:20:29 +0000

    Add user links and email confirmations via sendgrid

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -8,7 +8,8 @@ Rails.application.configure do
 
   # Do not eager load code on boot.
   config.eager_load = false
-
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { :host => "http://zippy-kings-landing-188897.nitrousapp.com"}
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
